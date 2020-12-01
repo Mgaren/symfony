401243ad7b48b492f73beb201effdfa99e20f51b
Mgaren <basketeur009@gmail.com>
2020-12-01 10:32:29 +0100

    quete symfony 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -17,4 +17,14 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
          ]);
     }
+
+    /**
+     * @Route ("/programs/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
 }
